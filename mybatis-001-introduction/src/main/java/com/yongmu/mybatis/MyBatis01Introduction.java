94d65140a4b3929015f0b0ed110a14791cfa801f
jiangfan <1565133958@qq.com>
2022-10-27 21:30:35 +0800

    01

---
+++ mybatis-001-introduction/src/main/java/com/yongmu/mybatis/MyBatis01Introduction.java
@@ -0,0 +1,12 @@
+package com.yongmu.mybatis;
+
+/**
+ * @author ：jiangfan
+ * @date ：2022/10/25 17:31
+ * @description ：入门程序
+ */
+public class MyBatis01Introduction {
+    public static void main(String[] args) {
+
+    }
+}
